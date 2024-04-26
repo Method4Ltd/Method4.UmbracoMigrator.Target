2395531674b0e7c65fd5be49a7c5cf729ac865c8
Owain Jones <owain.jones@method4.co.uk>
2024-04-26 15:19:57 +0100

    Updated to .NET 8 and updated dependencies

--- src/Method4.UmbracoMigrator.Target.SampleSite/Program.cs
+++ src/Method4.UmbracoMigrator.Target.SampleSite/Program.cs
@@ -1,19 +1,61 @@
-namespace Method4.UmbracoMigrator.Target.SampleSite
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-            => CreateHostBuilder(args)
-                .Build()
-                .Run();
+// Configure Builder
+var builder = WebApplication.CreateBuilder(args);
+ConfigureBuilder(builder);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureUmbracoDefaults()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStaticWebAssets();
-                    webBuilder.UseStartup<Startup>();
-                });
+// Configure App
+var app = builder.Build();
+await app.BootUmbracoAsync();
+ConfigureApp(app, app.Environment);
+await app.RunAsync();
+return;
+
+static void ConfigureBuilder(WebApplicationBuilder builder)
+{
+    builder.CreateUmbracoBuilder()
+        .AddBackOffice()
+        .AddWebsite()
+        .AddDeliveryApi()
+        .AddComposers()
+        //.AddAzureBlobMediaFileSystem() // This configures the required services for Media
+        //.AddAzureBlobImageSharpCache() // This configures the required services for the Image Sharp cache
+        .Build();
+}
+
+static void ConfigureApp(WebApplication app, IWebHostEnvironment env)
+{
+    if (env.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
     }
+    else
+    {
+        app.UseHsts();
+    }
+
+    app.Use(async (context, next) =>
+    {
+        // Click Jacking protection
+        context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+        // Content/MIME Sniffing Protection
+        context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+        await next();
+    });
+
+    // Configure Umbraco Services
+    app.UseUmbraco()
+        .WithMiddleware(u =>
+        {
+            u.UseBackOffice();
+            u.UseWebsite();
+        })
+        .WithEndpoints(u =>
+        {
+            u.UseInstallerEndpoints();
+            u.UseBackOfficeEndpoints();
+            u.UseWebsiteEndpoints();
+            u.EndpointRouteBuilder.MapControllers();
+        });
+
+    // Add static files to the request pipeline.
+    app.UseStaticFiles();
 }
