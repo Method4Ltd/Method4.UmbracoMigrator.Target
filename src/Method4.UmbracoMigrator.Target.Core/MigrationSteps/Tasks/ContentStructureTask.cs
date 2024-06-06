23e761e3b68d50e0964dd69051dfeb72e2a4eeb5
Owain Jones <owain.jones@method4.co.uk>
2024-06-06 12:20:54 +0100

    Changed to explicit constructors

--- src/Method4.UmbracoMigrator.Target.Core/MigrationSteps/Tasks/ContentStructureTask.cs
+++ src/Method4.UmbracoMigrator.Target.Core/MigrationSteps/Tasks/ContentStructureTask.cs
@@ -13,23 +13,42 @@ using Umbraco.Cms.Core.Strings;
 
 namespace Method4.UmbracoMigrator.Target.Core.MigrationSteps.Tasks
 {
-    internal class ContentStructureTask(IMappingCollectionService migrationMapperService,
-        IInternalDocTypeMapping defaultDocTypeMapper,
-        IContentTypeService contentTypeService,
-        IContentService contentService,
-        IRelationLookupService relationLookupService,
-        IShortStringHelper shortStringHelper,
-        ILogger<ContentStructureTask> logger,
-        IHubContext<MigrationHub> hubContext) : IContentStructureTask
+    internal class ContentStructureTask : IContentStructureTask
     {
-        private readonly HubService _hubService = new(hubContext);
+        private readonly HubService _hubService;
+        private readonly IMappingCollectionService _migrationMapperService;
+        private readonly IInternalDocTypeMapping _defaultDocTypeMapper;
+        private readonly IContentTypeService _contentTypeService;
+        private readonly IContentService _contentService;
+        private readonly IRelationLookupService _relationLookupService;
+        private readonly IShortStringHelper _shortStringHelper;
+        private readonly ILogger<ContentStructureTask> _logger;
+
+        public ContentStructureTask(IMappingCollectionService migrationMapperService,
+            IInternalDocTypeMapping defaultDocTypeMapper,
+            IContentTypeService contentTypeService,
+            IContentService contentService,
+            IRelationLookupService relationLookupService,
+            IShortStringHelper shortStringHelper,
+            ILogger<ContentStructureTask> logger,
+            IHubContext<MigrationHub> hubContext)
+        {
+            _migrationMapperService = migrationMapperService;
+            _defaultDocTypeMapper = defaultDocTypeMapper;
+            _contentTypeService = contentTypeService;
+            _contentService = contentService;
+            _relationLookupService = relationLookupService;
+            _shortStringHelper = shortStringHelper;
+            _logger = logger;
+            _hubService = new(hubContext);
+        }
 
         /// <inheritdoc />
         public void CreateContentNodeStructure(List<MigrationContent> nodesToMigrate)
         {
             var stopwatch = new Stopwatch();
             stopwatch.Start();
-            logger.LogInformation("Creating Content Structure");
+            _logger.LogInformation("Creating Content Structure");
 
             var failedDocTypes = new List<string>();
             nodesToMigrate = nodesToMigrate.OrderBy(x => x.Level).ToList();
@@ -44,15 +63,15 @@ namespace Method4.UmbracoMigrator.Target.Core.MigrationSteps.Tasks
                 var oldName = oldNode.Name.Truncate(20);
 
                 _hubService.SendMessage(1, $"Creating content node - {count}/{nodesToMigrate.Count} - \"{oldName}\"");
-                logger.LogDebug("Creating content node - {count}/{total} - \"{oldName}\"", count, nodesToMigrate.Count, oldName);
+                _logger.LogDebug("Creating content node - {count}/{total} - \"{oldName}\"", count, nodesToMigrate.Count, oldName);
 
-                var customMapping = migrationMapperService.GetDocTypeMapping(oldNode);
+                var customMapping = _migrationMapperService.GetDocTypeMapping(oldNode);
 
                 // If we can't map this node, then we will not create it
-                if (customMapping == null && defaultDocTypeMapper.CanIMap(oldNode) == false)
+                if (customMapping == null && _defaultDocTypeMapper.CanIMap(oldNode) == false)
                 {
                     _hubService.SendMessage(1, $"Could not create - \"{oldNode.Name.Truncate(20)}\" - No document type mapper exists for \"{oldNode.ContentType}\"");
-                    logger.LogWarning("No mapper exists for the \"{OldDocTypeAlias}\" DocType", oldNode.ContentType);
+                    _logger.LogWarning("No mapper exists for the \"{OldDocTypeAlias}\" DocType", oldNode.ContentType);
                     if (failedDocTypes.Contains(oldNode.ContentType) == false)
                     {
                         failedDocTypes.Add(oldNode.ContentType);
@@ -74,68 +93,68 @@ namespace Method4.UmbracoMigrator.Target.Core.MigrationSteps.Tasks
                     {
                         // 916724a5-173d-4619-b97e-b9de133dd6f5 is the GUID of the Umbraco Master Root - https://github.com/umbraco/Umbraco-CMS/blob/d8b0616434d1114d0628fb5702206bea39baa88b/src/Umbraco.Infrastructure/Migrations/Install/DatabaseDataCreator.cs#L178
                         // Trashed items can have this as it's parent key
-                        newNode = defaultDocTypeMapper.CreateRootNode(oldNode, contentType);
+                        newNode = _defaultDocTypeMapper.CreateRootNode(oldNode, contentType);
                     }
                     else
                     {
-                        var parentRelation = relationLookupService.GetRelationByOldKey(oldNode.ParentKey);
+                        var parentRelation = _relationLookupService.GetRelationByOldKey(oldNode.ParentKey);
                         if (parentRelation == null)
                         {
                             // Add as a child to the temp node, if we haven't yet migrated it's parent
-                            logger.LogWarning("Lookup for key '{parentKey}' Not found, '{oldKey}' will be added to the temp content node.", oldNode.ParentKey, oldNode.Key);
+                            _logger.LogWarning("Lookup for key '{parentKey}' Not found, '{oldKey}' will be added to the temp content node.", oldNode.ParentKey, oldNode.Key);
                             newNode = AddToTempParentNode(oldNode, contentType);
                         }
                         else
                         {
-                            newNode = defaultDocTypeMapper.CreateNode(oldNode, contentType, parentRelation.NewKeyAsGuid);
+                            newNode = _defaultDocTypeMapper.CreateNode(oldNode, contentType, parentRelation.NewKeyAsGuid);
                         }
                     }
                 }
                 else
                 {
-                    newNode = defaultDocTypeMapper.CreateRootNode(oldNode, contentType);
+                    newNode = _defaultDocTypeMapper.CreateRootNode(oldNode, contentType);
                 }
 
                 // Save
                 try
                 {
-                    contentService.Save(newNode);
+                    _contentService.Save(newNode);
                 }
                 catch (Exception ex)
                 {
-                    logger.LogError(ex, "Failed to save node {nodeName}. old node key: {oldNodeKey}", newNode.Name, oldNode.Key);
+                    _logger.LogError(ex, "Failed to save node {nodeName}. old node key: {oldNodeKey}", newNode.Name, oldNode.Key);
                     throw;
                 }
 
                 // Store new key lookup
-                relationLookupService.StoreNewRelation(
+                _relationLookupService.StoreNewRelation(
                     newNode.Id.ToString(),
                     oldNode.Id,
                     newNode.Key,
                     oldNode.Key);
 
-                logger.LogDebug("Created content node - {count}/{total} - \"{oldName}\" - {elapsedTime}", count, nodesToMigrate.Count, oldName, nodeStopwatch.Humanize());
+                _logger.LogDebug("Created content node - {count}/{total} - \"{oldName}\" - {elapsedTime}", count, nodesToMigrate.Count, oldName, nodeStopwatch.Humanize());
             }
 
             if (failedDocTypes.Any())
             {
-                logger.LogWarning("Failed to create nodes for the following DocTypes, as no mappers exist for them: \"{OldDocTypeAliases}\"", string.Join(',', failedDocTypes));
+                _logger.LogWarning("Failed to create nodes for the following DocTypes, as no mappers exist for them: \"{OldDocTypeAliases}\"", string.Join(',', failedDocTypes));
             }
 
             stopwatch.Stop();
             _hubService.SendMessage(1, $"Created Content Structure - {stopwatch.Humanize()}");
-            logger.LogInformation("Created Content Structure - {elapsedTime}", stopwatch.Humanize());
+            _logger.LogInformation("Created Content Structure - {elapsedTime}", stopwatch.Humanize());
         }
 
         private bool DoesNodeAlreadyExist(Guid oldKey)
         {
             // Do we have a key lookup entry?
-            var relation = relationLookupService.GetRelationByOldKey(oldKey);
+            var relation = _relationLookupService.GetRelationByOldKey(oldKey);
             var newKey = relation?.NewKeyAsGuid;
             if (newKey == null) { return false; }
 
             // Does a content node with the key exist?
-            var contentNode = contentService.GetById((Guid)newKey);
+            var contentNode = _contentService.GetById((Guid)newKey);
             if (contentNode != null)
             {
                 return true;
@@ -147,28 +166,28 @@ namespace Method4.UmbracoMigrator.Target.Core.MigrationSteps.Tasks
         private IContent AddToTempParentNode(MigrationContent oldNode, string contentType)
         {
             _hubService.SendMessage(1, $"Saving '{oldNode.Key}' under temp node");
-            logger.LogWarning("Saving '{oldKey}' under temp node", oldNode.Key);
+            _logger.LogWarning("Saving '{oldKey}' under temp node", oldNode.Key);
             var tempParent = GetOrCreateTempParentContentNode();
-            return defaultDocTypeMapper.CreateNode(oldNode, contentType, tempParent.Key);
+            return _defaultDocTypeMapper.CreateNode(oldNode, contentType, tempParent.Key);
         }
 
         private IContent GetOrCreateTempParentContentNode()
         {
-            var tempParent = contentService.GetByLevel(1).FirstOrDefault(c => c.ContentType.Alias == "migrationTempParentNode");
+            var tempParent = _contentService.GetByLevel(1).FirstOrDefault(c => c.ContentType.Alias == "migrationTempParentNode");
 
             // Create the temp node if it doesn't exist
             if (tempParent == null)
             {
                 // Create DocType if it doesn't exist
-                var tempContentType = contentTypeService.Get("migrationTempParentNode");
+                var tempContentType = _contentTypeService.Get("migrationTempParentNode");
                 if (tempContentType == null)
                 {
-                    tempContentType = new ContentType(shortStringHelper, -1) { Alias = "migrationTempParentNode", Icon = "icon-truck color-deep-purple" };
+                    tempContentType = new ContentType(_shortStringHelper, -1) { Alias = "migrationTempParentNode", Icon = "icon-truck color-deep-purple" };
                     tempContentType.Name = "Migration Temporary Parent Node";
-                    contentTypeService.Save(tempContentType);
+                    _contentTypeService.Save(tempContentType);
                 }
 
-                tempParent = contentService.CreateAndSave("Migration Temp Parent", -1, "migrationTempParentNode");
+                tempParent = _contentService.CreateAndSave("Migration Temp Parent", -1, "migrationTempParentNode");
             }
 
             return tempParent;
--- src/Method4.UmbracoMigrator.Target.Core/MigrationSteps/Tasks/MediaStructureTask.cs
+++ src/Method4.UmbracoMigrator.Target.Core/MigrationSteps/Tasks/MediaStructureTask.cs
@@ -13,23 +13,42 @@ using Umbraco.Cms.Core.Strings;
 
 namespace Method4.UmbracoMigrator.Target.Core.MigrationSteps.Tasks
 {
-    internal class MediaStructureTask(IMappingCollectionService migrationMapperService,
-        IInternalMediaTypeMapping defaultMediaTypeMapper,
-        IMediaTypeService mediaTypeService,
-        IMediaService mediaService,
-        IRelationLookupService relationLookupService,
-        IShortStringHelper shortStringHelper,
-        ILogger<MediaStructureTask> logger,
-        IHubContext<MigrationHub> hubContext) : IMediaStructureTask
+    internal class MediaStructureTask : IMediaStructureTask
     {
-        private readonly HubService _hubService = new(hubContext);
+        private readonly HubService _hubService;
+        private readonly IMappingCollectionService _migrationMapperService;
+        private readonly IInternalMediaTypeMapping _defaultMediaTypeMapper;
+        private readonly IMediaTypeService _mediaTypeService;
+        private readonly IMediaService _mediaService;
+        private readonly IRelationLookupService _relationLookupService;
+        private readonly IShortStringHelper _shortStringHelper;
+        private readonly ILogger<MediaStructureTask> _logger;
+
+        public MediaStructureTask(IMappingCollectionService migrationMapperService,
+            IInternalMediaTypeMapping defaultMediaTypeMapper,
+            IMediaTypeService mediaTypeService,
+            IMediaService mediaService,
+            IRelationLookupService relationLookupService,
+            IShortStringHelper shortStringHelper,
+            ILogger<MediaStructureTask> logger,
+            IHubContext<MigrationHub> hubContext)
+        {
+            _migrationMapperService = migrationMapperService;
+            _defaultMediaTypeMapper = defaultMediaTypeMapper;
+            _mediaTypeService = mediaTypeService;
+            _mediaService = mediaService;
+            _relationLookupService = relationLookupService;
+            _shortStringHelper = shortStringHelper;
+            _logger = logger;
+            _hubService = new(hubContext);
+        }
 
         /// <inheritdoc />
         public void CreateMediaNodeStructure(List<MigrationMedia> nodesToMigrate)
         {
             var stopwatch = new Stopwatch();
             stopwatch.Start();
-            logger.LogInformation("Creating Media Structure");
+            _logger.LogInformation("Creating Media Structure");
 
             var failedMediaTypes = new List<string>();
             nodesToMigrate = nodesToMigrate.OrderBy(x => x.Level).ToList();
@@ -44,15 +63,15 @@ namespace Method4.UmbracoMigrator.Target.Core.MigrationSteps.Tasks
                 var oldName = oldNode.Name.Truncate(20);
 
                 _hubService.SendMessage(1, $"Creating media node - {count}/{nodesToMigrate.Count} - \"{oldName}\"");
-                logger.LogDebug("Creating media node - {count}/{total} - \"{oldName}\"", count, nodesToMigrate.Count, oldName);
+                _logger.LogDebug("Creating media node - {count}/{total} - \"{oldName}\"", count, nodesToMigrate.Count, oldName);
 
-                var customMapping = migrationMapperService.GetMediaTypeMapping(oldNode);
+                var customMapping = _migrationMapperService.GetMediaTypeMapping(oldNode);
 
                 // If we can't map this node, then we will not create it
-                if (customMapping == null && defaultMediaTypeMapper.CanIMap(oldNode) == false)
+                if (customMapping == null && _defaultMediaTypeMapper.CanIMap(oldNode) == false)
                 {
                     _hubService.SendMessage(1, $"Could not create - \"{oldNode.Name.Truncate(20)}\" - No media type mapper exists for \"{oldNode.ContentType}\"");
-                    logger.LogWarning("No mapper exists for the \"{OldMediaTypeAlias}\" MediaType", oldNode.ContentType);
+                    _logger.LogWarning("No mapper exists for the \"{OldMediaTypeAlias}\" MediaType", oldNode.ContentType);
                     if (failedMediaTypes.Contains(oldNode.ContentType) == false)
                     {
                         failedMediaTypes.Add(oldNode.ContentType);
@@ -74,97 +93,97 @@ namespace Method4.UmbracoMigrator.Target.Core.MigrationSteps.Tasks
                     {
                         // 916724a5-173d-4619-b97e-b9de133dd6f5 is the GUID of the Umbraco Master Root - https://github.com/umbraco/Umbraco-CMS/blob/d8b0616434d1114d0628fb5702206bea39baa88b/src/Umbraco.Infrastructure/Migrations/Install/DatabaseDataCreator.cs#L178
                         // Trashed items can have this as it's parent key
-                        newNode = defaultMediaTypeMapper.CreateRootNode(oldNode, contentType);
+                        newNode = _defaultMediaTypeMapper.CreateRootNode(oldNode, contentType);
                     }
                     else
                     {
-                        var parentRelation = relationLookupService.GetRelationByOldKey(oldNode.ParentKey);
+                        var parentRelation = _relationLookupService.GetRelationByOldKey(oldNode.ParentKey);
                         if (parentRelation == null)
                         {
 
                             // Add as a child to the temp node, if we haven't yet migrated it's parent
-                            logger.LogWarning("Lookup for key '{parentKey}' Not found, '{oldKey}' will be added to the temp media node.", oldNode.ParentKey, oldNode.Key);
+                            _logger.LogWarning("Lookup for key '{parentKey}' Not found, '{oldKey}' will be added to the temp media node.", oldNode.ParentKey, oldNode.Key);
                             newNode = AddToTempParentNode(oldNode, contentType);
                         }
                         else
                         {
-                            newNode = defaultMediaTypeMapper.CreateNode(oldNode, contentType, parentRelation.NewKeyAsGuid);
+                            newNode = _defaultMediaTypeMapper.CreateNode(oldNode, contentType, parentRelation.NewKeyAsGuid);
                         }
                     }
                 }
                 else
                 {
-                    newNode = defaultMediaTypeMapper.CreateRootNode(oldNode, contentType);
+                    newNode = _defaultMediaTypeMapper.CreateRootNode(oldNode, contentType);
                 }
 
                 // Save
                 try
                 {
-                    mediaService.Save(newNode);
+                    _mediaService.Save(newNode);
                 }
                 catch (Exception ex)
                 {
-                    logger.LogError(ex, "Failed to save node {nodeName}. old node key: {oldNodeKey}", newNode.Name, oldNode.Key);
+                    _logger.LogError(ex, "Failed to save node {nodeName}. old node key: {oldNodeKey}", newNode.Name, oldNode.Key);
                     throw;
                 }
 
                 // Store new key lookup
-                relationLookupService.StoreNewRelation(
+                _relationLookupService.StoreNewRelation(
                     newNode.Id.ToString(),
                     oldNode.Id,
                     newNode.Key,
                     oldNode.Key);
 
-                logger.LogDebug("Created media node - {count}/{total} - \"{oldName}\" - {elapsedTime}", count, nodesToMigrate.Count, oldName, nodeStopwatch.Humanize());
+                _logger.LogDebug("Created media node - {count}/{total} - \"{oldName}\" - {elapsedTime}", count, nodesToMigrate.Count, oldName, nodeStopwatch.Humanize());
             }
 
             if (failedMediaTypes.Any())
             {
-                logger.LogWarning("Failed to create nodes for the following MediaTypes, as no mappers exist for them: \"{OldDocTypeAliases}\"", string.Join(',', failedMediaTypes));
+                _logger.LogWarning("Failed to create nodes for the following MediaTypes, as no mappers exist for them: \"{OldDocTypeAliases}\"", string.Join(',', failedMediaTypes));
             }
 
             stopwatch.Stop();
             _hubService.SendMessage(1, $"Created Media Structure - {stopwatch.Humanize()}");
-            logger.LogInformation("Created Media Structure - {elapsedTime}", stopwatch.Humanize());
+            _logger.LogInformation("Created Media Structure - {elapsedTime}", stopwatch.Humanize());
         }
 
         private bool DoesNodeAlreadyExist(Guid oldKey)
         {
             // Do we have a key lookup entry?
-            var relation = relationLookupService.GetRelationByOldKey(oldKey);
+            var relation = _relationLookupService.GetRelationByOldKey(oldKey);
             var newKey = relation?.NewKeyAsGuid;
             if (newKey == null) { return false; }
 
             // Does a media node with the key exist?
-            var mediaNode = mediaService.GetById((Guid)newKey);
+            var mediaNode = _mediaService.GetById((Guid)newKey);
             return mediaNode != null;
         }
 
         private IMedia AddToTempParentNode(MigrationMedia oldNode, string contentType)
         {
             _hubService.SendMessage(1, $"Saving '{oldNode.Key}' under temp node");
-            logger.LogWarning("Saving '{oldKey}' under temp node", oldNode.Key);
+            _logger.LogWarning("Saving '{oldKey}' under temp node", oldNode.Key);
             var tempParent = GetOrCreateTempParentMediaNode();
-            return defaultMediaTypeMapper.CreateNode(oldNode, contentType, tempParent.Key);
+            return _defaultMediaTypeMapper.CreateNode(oldNode, contentType, tempParent.Key);
         }
 
         private IMedia GetOrCreateTempParentMediaNode()
         {
-            var tempParent = mediaService.GetByLevel(1)?.FirstOrDefault(c => c.ContentType.Alias == "migrationTempParentNode");
+            var tempParent = _mediaService.GetByLevel(1)?.FirstOrDefault(c => c.ContentType.Alias == "migrationTempParentNode");
 
             // Create the temp node if it doesn't exist
             if (tempParent == null)
             {
                 // Create DocType if it doesn't exist
-                var tempContentType = mediaTypeService.Get("migrationTempParentNode");
+                var tempContentType = _mediaTypeService.Get("migrationTempParentNode");
                 if (tempContentType == null)
                 {
-                    tempContentType = new MediaType(shortStringHelper, -1) { Alias = "migrationTempParentNode", Icon = "icon-truck color-deep-purple" };
+                    tempContentType = new MediaType(_shortStringHelper, -1) { Alias = "migrationTempParentNode", Icon = "icon-truck color-deep-purple" };
                     tempContentType.Name = "Migration Temporary Parent Node";
-                    mediaTypeService.Save(tempContentType);
+                    _mediaTypeService.Save(tempContentType);
                 }
 
-                tempParent = mediaService.CreateMediaWithIdentity("Migration Temp Parent", -1, "migrationTempParentNode");
+                tempParent = _mediaService.CreateMediaWithIdentity("Migration Temp Parent", -1, "migrationTempParentNode");
             }
 
             return tempParent;
